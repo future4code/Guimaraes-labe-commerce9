9b5d2cd2d671d65efd84b28e3675c9c01799376f
FRANCINEIDETS <francineide_silva@hotmail.com>
2022-02-17 21:31:57 -0300

    ciacao-da-pasta-data-com-produtos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import styled from 'styled-components';
 import Header from './components/Header';
 import Produtos from './components/Produtos/Produtos';
+import toyLista from "./material/toys.json";
+import shirtsLista from "./material/shirts.json";
+
 
 
 const Container = styled.div`
@@ -13,72 +16,11 @@ const Container = styled.div`
 class App extends React.Component{
 
   state = {
-    produtos: [
-      { id: 1,
-        nome: 'Arma',
-        value: 150.00,
-        imageUrl: "https://i.ibb.co/9h25HKP/arma.jpg"
-      },
-    
-      {
-        id: 2,
-        nome: ' Ioda',
-        value : 559.00,
-        imageUrl: 'https://i.ibb.co/5GSxrSy/BONECO1.jpg'
-      },
-    
-      {
-        id: 3,
-        nome: 'FURBACCA',
-        value: 799.00,
-        imageUrl: 'https://i.ibb.co/MctrwRh/FURBACCA.jpg'
-      },
-    
-      {
-        id: 4,
-        nome: 'HASBRO',
-        value: 999.00,
-        imageUrl: 'https://i.ibb.co/Lp8sBSw/HASBRO.jpg'
-      },
-    
-      {
-        id: 5,
-        nome:'Mascara',
-        value: 250.00,
-        imageUrl: 'https://i.ibb.co/wrsJ8ny/MASCARA.jpg'
-      },
-    
-      {
-        id: 6,
-        nome: 'NAVE X',
-        value: 1593.00,
-        imageUrl:'https://i.ibb.co/ctt26YN/NAVE-X.jpg'
-      },
-    
-      {
-        id: 7,
-        nome: 'Strormtroope',
-        value: 450.00,
-        imageUrl:'https://i.ibb.co/WDscf5T/STORMTROOPE.jpg'
-      },
-    
-      {
-        id: 8,
-        nome: 'DARTH VADER',
-        value: 899.00,
-        imageUrl: 'https://i.ibb.co/6PMyVgc/DARTH-VADER.jpg'
-      },
-    ],
+   toys: toyLista,
+   shirts: shirtsLista,
+      
 
-    carrinho: [
-      {
-      id: '',
-      nome: '',
-      value: '',
-      imageUrl:'',
-      quantidade:''
-      }
-    ]
+    
   }  
   
 
--- src/components/Produtos/CardDeProdutos.jsx
+++ src/components/Produtos/CardDeProdutos.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import toyLista from ""
 
 class CardDeProdutos extends React.Component{
 
